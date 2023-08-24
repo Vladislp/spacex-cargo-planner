73f575de03013e6f7c3c082187982facaaf5c996
Vladislp <42935979+Vladislp@users.noreply.github.com>
2023-08-24 13:38:25 +0300

    3/2-Shipment-List-Styling-SVG-Error
    * Condition check for boxUnits === null
    * Minor css modifications px --> em
    * Adding svg logo to project
    * Updating shipment list
          ~ Logo
          ~ Search bar working by company names
    * Updating shipment details
          ~ Update boxUnits and cargo bays when the shipment prop changes

--- spacex-cargo-planner/components/ShipmentsList.js
+++ spacex-cargo-planner/components/ShipmentsList.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import ShipmentDetails from './ShipmentDetails';
+import { FaSearch } from 'react-icons/fa';
 
 const ShipmentList = () => {
     const [shipments, setShipments] = useState([]);
     const [selectedShipmentId, setSelectedShipmentId] = useState(null);
     const [selectedShipment, setSelectedShipment] = useState(null); // New state for selected shipment
+    const [searchQuary, setSearchQuary] = useState("");
+    const filteredShipments = shipments.filter((shipment) =>
+        shipment.name.toLowerCase().includes(searchQuary.toLowerCase())
+    );
 
     useEffect(() => {
         const fetchData = async () => {
@@ -19,14 +24,25 @@ const ShipmentList = () => {
         setSelectedShipmentId(id);
         const selected = shipments.find((shipment) => shipment.id === id);
         setSelectedShipment(selected);
+        setSearchQuary("");
     };
 
     return (
+        
         <div className="shipment-list-container">
-            <h1>Shiping List</h1>
+            <img className='spacex-logo' src="/logo.svg" alt="Logo" />
+                <input
+                    type="search"
+                    className='search'
+                    value={searchQuary}
+                    onChange={(e) => setSearchQuary(e.target.value)}
+                    list="box-options"
+                    placeholder="Search"
+                />
+                
             <table> 
                 <tbody>
-                    {shipments.map((shipment) => (
+                    {filteredShipments.map((shipment) => (
                         <tr
                             key={shipment.id}
                             onClick={() => handleRowClick(shipment.id)}
--- spacex-cargo-planner/utils/cargoUtils.js
+++ spacex-cargo-planner/utils/cargoUtils.js
@@ -1,4 +1,8 @@
 export const calculateCargoBays = (boxUnits) => {
+    if (!boxUnits) {
+        return 0; // Return a default value or handle the case where boxUnits is null
+    }
+
     const units = boxUnits.split(',').map(parseFloat);
     const totalUnits = units.reduce((total, unit) => total + unit, 0);
     const cargoBays = Math.ceil(totalUnits / 10);
