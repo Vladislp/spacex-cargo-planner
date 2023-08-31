a6b95c2b5b27d09dba5234888c53a3cf5fdb4e1a
Vladislp <42935979+Vladislp@users.noreply.github.com>
2023-08-31 12:56:40 +0300

    11-A11y-Features-Implementation
    - Semantic HTML: Went through the project, everything should be as intended.
      - Alternative Text for Images: Well, i only have one image in this project.
      - Keyboard Navigation: Implemented keyboard navigation, so that people can browse project using only keyboard
      - Focus Styles: Provided clear and visible focus styles for interactive elements when they receive keyboard focus.
      - Color Contrast: That's kinda hard to implement, because it requires real-world testing and feedback
      - Semantic Forms: Went through the project, everything should be as intended.
      - ARIA Roles and Attributes: Added some ARIA features to project, where it is fit for me.
      - Skip Links: Well, it's kinda feels off to implement this here, because what considered a main-content.
      - Screen Reader Testing: For now seems f2m.
      - Responsive Design: Part of the "UI and Functionality" section.
      - Video and Audio Accessibility: Not relevent.
      - Keyboard Shortcuts: I can't really come up with something in this type of application

--- spacex-cargo-planner/pages/index.tsx
+++ spacex-cargo-planner/pages/index.tsx
@@ -5,6 +5,10 @@ import ShipmentList from '../components/ShipmentsList/ShipmentsList';
 const Home = () => {
     return (
         <div>
+            {/* Skip to content link */}
+            <a href='#main-content' className='skip-link'>
+                Skip to content
+            </a>
             <RectangleComponent />
             <ShipmentList />
         </div>
